f28e96c3d50eb27267d25d7aaf609d0919f9dc1c
DmytroTHR <dtyagur@gmail.com>
2022-06-09 23:35:06 +0300

    deploy on minikube

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
-const webPort = "8080"
+const webPort = "8088"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -21,23 +23,36 @@ func main() {
 	}
 }
 
+//go:embed templates
+var templateFS embed.FS
+
 func render(w http.ResponseWriter, t string) {
 
 	templateSlice := []string{
-		"./cmd/web/templates/base.layout.gohtml",
-		"./cmd/web/templates/header.partial.gohtml",
-		"./cmd/web/templates/footer.partial.gohtml",
+		"templates/base.layout.gohtml",
+		"templates/header.partial.gohtml",
+		"templates/footer.partial.gohtml",
 	}
 
-	templateSlice = append([]string{fmt.Sprintf("./cmd/web/templates/%s", t)}, templateSlice...)
+	templateSlice = append([]string{fmt.Sprintf("templates/%s", t)}, templateSlice...)
 
-	tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFS(templateFS, templateSlice...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if err := tmpl.Execute(w, nil); err != nil {
+	var dataTmpl struct {
+		BrokerURL string
+	}
+
+	// brokerHost := "10.109.224.181"
+	// brokerPort := "8080"
+	// dataTmpl.BrokerURL = "http://" + brokerHost + ":" + brokerPort
+
+	dataTmpl.BrokerURL = os.Getenv("BROKER_URL")
+
+	if err := tmpl.Execute(w, dataTmpl); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
